7df37f911bf51e6af6d12c1aef466bf02c88c5aa
eray <eray@inuits.eu>
2023-01-05 18:39:21 +0100

    Add package documentation

--- inuits_policy_based_auth/__init__.py
+++ inuits_policy_based_auth/__init__.py
@@ -1,3 +1,27 @@
+"""
+Module for securing API endpoints based on policies.
+
+This module provides a generic and highly customizable way of securing
+API endpoints based on policies using a simple decorator.
+When dynamically loading policies from a custom configuration file, this
+module will allow you to add new policies and override existing ones
+to make it exactly fit your needs.
+
+Classes
+-------
+    Authenticator
+        used to authenticate a user
+    BasePolicy
+        used as an interface for concrete implementations of policies
+    Context
+        contains data about the context of a request
+    PolicyFactory
+        the main class, used to apply policies
+    UserAuthData
+        contains data about the authenticated user
+"""
+
+
 from inuits_policy_based_auth.authentication.authenticator import Authenticator
 from inuits_policy_based_auth.authentication.user_auth_data import UserAuthData
 from inuits_policy_based_auth.authorization.base_policy import BasePolicy
