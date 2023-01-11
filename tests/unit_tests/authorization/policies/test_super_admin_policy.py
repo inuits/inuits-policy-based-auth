4d9a66d714f74f95ec21e96ae9ef6dbc8f89f040
eray <eray@inuits.eu>
2023-01-11 14:59:51 +0100

    Add tests for super_admin_policy

---
+++ tests/unit_tests/authorization/policies/test_super_admin_policy.py
@@ -0,0 +1,26 @@
+from inuits_policy_based_auth.authorization.policies.super_admin_policy import (
+    SuperAdminPolicy,
+)
+from inuits_policy_based_auth.contexts import PolicyContext, RequestContext, UserContext
+
+
+class TestSuperAdminPolicy:
+    def setup_method(self):
+        self.policy_context = PolicyContext()
+        self.user_context = UserContext()
+        self.request_context = RequestContext(None)
+        self.super_admin_policy = SuperAdminPolicy()
+
+    def test_authorize_allows_access(self):
+        self.user_context.roles = ["role_super_admin"]
+        policy_context = self.super_admin_policy.authorize(
+            self.policy_context, self.user_context, self.request_context
+        )
+        assert policy_context.access_verdict == True
+
+    def test_authorize_does_not_determine_access(self):
+        self.user_context.roles = ["regular_user"]
+        policy_context = self.super_admin_policy.authorize(
+            self.policy_context, self.user_context, self.request_context
+        )
+        assert policy_context.access_verdict == None
