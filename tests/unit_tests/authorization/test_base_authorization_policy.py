af55efeb15bfc5b452da7e92614dcc9718aaa671
eray <eray@inuits.eu>
2023-01-11 11:43:38 +0100

    Add test for base_authorization_policy

---
+++ tests/unit_tests/authorization/test_base_authorization_policy.py
@@ -0,0 +1,58 @@
+import pytest
+
+from inuits_policy_based_auth.authorization.base_authorization_policy import (
+    BaseAuthorizationPolicy,
+)
+from inuits_policy_based_auth.contexts import PolicyContext, RequestContext, UserContext
+from inuits_policy_based_auth.exceptions import (
+    AuthorizeMethodDidNotReturnObjectOfTypePolicyContextException,
+)
+from unittest.mock import Mock, patch
+
+
+class TestBaseAuthorizationPolicy:
+    @patch(
+        "inuits_policy_based_auth.authorization.base_authorization_policy.BaseAuthorizationPolicy.__abstractmethods__",
+        set(),
+    )
+    def setup_method(self, method):
+        self.policy = BaseAuthorizationPolicy()  # type: ignore
+        self.spy_authorize_method = Mock(BaseAuthorizationPolicy())  # type: ignore
+        self.policy_context = PolicyContext()
+        self.request_context = RequestContext(None)
+        self.user_context = UserContext()
+
+        self.policy.authorize = self.spy_authorize_method
+
+    def test_apply_returns_policy_context(self):
+        self.policy.authorize.return_value = self.policy_context
+
+        policy_context = self.policy.apply(
+            self.policy_context, self.user_context, self.request_context
+        )
+
+        self.policy.authorize.assert_called_once()
+        assert policy_context == self.policy_context
+
+    def test_apply_sets_policy_context_access_verdict_to_none_by_default(self):
+        self.policy_context.access_verdict = True
+        self.policy.authorize.return_value = self.policy_context
+
+        policy_context = self.policy.apply(
+            self.policy_context, self.user_context, self.request_context
+        )
+
+        self.policy.authorize.assert_called_once()
+        assert policy_context.access_verdict == None
+
+    def test_apply_raises_AuthorizeMethodDidNotReturnObjectOfTypePolicyContextException(
+        self,
+    ):
+        with pytest.raises(
+            AuthorizeMethodDidNotReturnObjectOfTypePolicyContextException
+        ):
+            self.policy.apply(
+                self.policy_context, self.user_context, self.request_context
+            )
+
+        self.policy.authorize.assert_called_once()
