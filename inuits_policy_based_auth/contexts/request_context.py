e9ad73c7774ef251c3f9ba0739a6eab70ce6dc0a
eray <eray@inuits.eu>
2023-03-20 11:02:41 +0100

    Turn resource_scope into an array

--- inuits_policy_based_auth/contexts/request_context.py
+++ inuits_policy_based_auth/contexts/request_context.py
@@ -8,22 +8,22 @@ class RequestContext:
     ----------
     http_request : Unknown
         an HTTP request that is used by policies to determine access to a resource
-    resource_scope : str, optional
-        a scope a user must have for accessing a protected resource when policies rely on scope-based authorization
+    resource_scopes : list[str], optional
+        scopes a user must have for accessing a protected resource when policies rely on scope-based authorization
     """
 
-    def __init__(self, http_request, resource_scope=""):
+    def __init__(self, http_request, resource_scopes: list[str] = []):
         """
         Parameters
         ----------
         http_request : Unknown
             an HTTP request that is used by policies to determine access to a resource.
-        resource_scope : str, optional
-            a scope a user must have for accessing a protected resource when policies rely on scope-based authorization
+        resource_scopes : str, optional
+            scopes a user must have for accessing a protected resource when policies rely on scope-based authorization
         """
 
         self._http_request = http_request
-        self._resource_scope = resource_scope
+        self._resource_scopes = resource_scopes
 
     @property
     def http_request(self):
@@ -32,7 +32,7 @@ class RequestContext:
         return self._http_request
 
     @property
-    def resource_scope(self):
+    def resource_scopes(self):
         """A scope a user must have for accessing a protected resource when policies rely on scope-based authorization."""
 
-        return self._resource_scope
+        return self._resource_scopes
