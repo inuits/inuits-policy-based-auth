f205c58cf18f07561618ca4528fe7f285a7ee47d
Gijs Vermeulen <gverm@inuits.eu>
2023-04-12 18:38:28 +0200

    Don't hardcode super admin role

--- inuits_policy_based_auth/authorization/policies/super_admin_policy.py
+++ inuits_policy_based_auth/authorization/policies/super_admin_policy.py
@@ -1,3 +1,5 @@
+import os
+
 from inuits_policy_based_auth.authorization.base_authorization_policy import (
     BaseAuthorizationPolicy,
 )
@@ -8,8 +10,10 @@ class SuperAdminPolicy(BaseAuthorizationPolicy):
     An authorization policy that allows a user to do anything if he/she has the role 'role_super_admin'.
     """
 
+    super_admin_role: str = os.getenv("SUPER_ADMIN_ROLE", "super_admin")
+
     def authorize(self, policy_context, user_context, request_context):
-        if "role_super_admin" in user_context.roles:
+        if self.super_admin_role in user_context.roles:
             policy_context.access_verdict = True
 
         return policy_context
