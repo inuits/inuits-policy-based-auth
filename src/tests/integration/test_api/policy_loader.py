3b2dfe9260418ac072bf4ccf73d84b256f812592
Eray Özcan <eray@inuits.eu>
2023-08-29 11:05:23 +0200

    Fix broken test environment

--- src/tests/integration/test_api/policy_loader.py
+++ src/tests/integration/test_api/policy_loader.py
@@ -60,12 +60,9 @@ def __instantiate_authentication_policy(policy_module_name, policy, logger: Logg
     if policy_module_name == "token_based_policies.authlib_flask_oauth2_policy":
         return policy(
             logger,
-            os.getenv("STATIC_ISSUER", False),
-            os.getenv("STATIC_PUBLIC_KEY", False),
-            os.getenv("REALMS", "").split(","),
             os.getenv("ROLE_SCOPE_MAPPING", os.getenv("TEST_API_SCOPES")),
-            os.getenv("REMOTE_TOKEN_VALIDATION", False) in ["True", "true", True],
-            os.getenv("REMOTE_PUBLIC_KEY", False),
+            os.getenv("STATIC_ISSUER"),
+            os.getenv("STATIC_PUBLIC_KEY"),
         )
 
     return policy()
